37b13e9b206f5642117fe6dc69513f7bd2ac04fd
Sachin Pande <sazap10@gmail.com>
2016-02-02 21:16:53 +0000

    added envalid, express to dependencies and created simple webserver with hello world page

---
+++ server.js
@@ -0,0 +1,36 @@
+'use strict';
+
+// Dependencies
+const env = require('envalid'),
+    express = require('express'),
+    path = require('path'),
+    app = express();
+
+env.validate(process.env, {
+    PORT: {required: false, parse: env.toNumber}
+});
+
+const port = process.env.PORT || 3000;
+app.set('port', port);
+
+const oneDay = 86400000;
+
+app.use(express.static(path.join(__dirname, 'public'), {maxAge: oneDay}));
+
+app.use((error, req, res, next) => {
+    if (error) {
+        res.json('Error validating json');
+    }
+    else {
+        next();
+    }
+});
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.sendStatus(500);
+});
+
+app.listen(app.get('port'), () => {
+    console.log('Express server listening on port ' + app.get('port'));
+});
