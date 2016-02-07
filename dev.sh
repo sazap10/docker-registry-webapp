7f69768b099423a0efaa724013e8e59f002c774d
Sachin Pande <sazap10@gmail.com>
2016-02-07 22:12:46 +0000

    added routes for repository and images tags using sammy, added pure css

--- dev.sh
+++ dev.sh
@@ -1,6 +1,6 @@
 export PORT=3000
 
-export DOCKER_REGISTRY_HOST=[registry hostname with proto]
-export DOCKER_REGISTRY_PORT=[registry port]
+export DOCKER_REGISTRY_HOST=https://registry:aaC8QIQ9ToRTC6O9bhez0TjAgoc0OONX@registry.azure.sachinpan.de
+export DOCKER_REGISTRY_PORT=443
 
 nodemon server.js
